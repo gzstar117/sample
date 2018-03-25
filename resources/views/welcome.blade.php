d3142088e7e85d72815d44adbbf66e7c77775d75
Star <gzstar117@aliyun.com>
2018-03-25 10:41:56 +0000

    配置好heroKu

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hello~WeiBo~
+                    Hello~WeiBo In StarKyle~
                 </div>
 
                 <div class="links">
